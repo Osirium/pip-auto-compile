728313482999fc1981a48273b9069ae946eec548
Samathy Barratt <samathy.barratt@osirium.com>
2020-07-08 11:15:49 +0100

    We don't need argparse

--- pipautocompile.py
+++ pipautocompile.py
@@ -1,21 +1,14 @@
 #/usr/bin/env python
-import argparse
 import sys
-import subprocess
 from piptools.scripts import compile
 
 def compile_file(filename):
-    return compile.cli(["-r", filename, "-o", filename[0:-2]+".txt", "--verbose", "--generate-hashes"])
+    return compile.cli(["-r", filename, "-o", filename[0:-2]+"txt", "--verbose", "--generate-hashes"])
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("language", nargs="+")
-    parser.add_argument("language_version", nargs="+")
-    parser.add_argument("files", nargs="+")
+    files = sys.argv[1:]
 
-    args = parser.parse_args(sys.argv)
-
-    for f in args.files:
+    for f in files:
         if not compile_file(f):
             return False
 
