1beb8ccb93836b9a8cebbc32c8279ba98e06eeab
Samathy Barratt <samathy.barratt@osirium.com>
2020-07-09 11:08:07 +0100

    Catch SystemExit instead of monkey patching sys.exit away

--- pipautocompile.py
+++ pipautocompile.py
@@ -3,18 +3,6 @@ import sys
 from piptools.scripts import compile
 from piptools.exceptions import PipToolsError
 
-def monkey_exit(val):
-    ''' pip.compile.cli is not meant to be called by a script.
-        So it calls sys.exit() when its done, or on error, 
-        which kills the script before we can finish nicely.
-        Better turn that exit() into an exception that we can catch instead
-    '''
-    if( val != 0 and val is not None ):
-        raise PipToolsError(val)
-    return
-
-compile.sys.exit = monkey_exit
-
 def compile_file(filename):
     compile.cli(["-r", filename, "-o", filename[0:-2]+"txt", "--verbose", "--generate-hashes"])
 
@@ -30,19 +18,22 @@ def main():
                 try:
                     print("Compiling {}".format(requirementsin))
                     compile_file(requirementsin)
-                except PipToolsError:
-                    print("Attempted to compile maching `.in` to {}".format(f))
-                    print("Could not compile {}".format(requirementsin))
-                    print("Perhaps {} doesnt exist".format(requirementsin))
+                except SystemExit as e:
+                    if (e.code > 0):
+                        print("Attempted to compile maching `.in` to {}".format(f))
+                        print("Could not compile {}".format(requirementsin))
+                        print("Perhaps {} doesnt exist".format(requirementsin))
                     continue
         else:
             print("non txt file")
             try:
                 compile_file(f)
-            except PipToolsError as e:
-                print(e)
-                print("Could not compile {}".format(f))
-                return False
+            except SystemExit as e:
+                if (e.code >= 0):
+                    print(e)
+                    print("Could not compile {}".format(f))
+                    return False
+                continue
 
 if __name__ == "__main__":
     main()
