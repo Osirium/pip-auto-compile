d8a762917ebd4455e946c1f736366fae47711b2f
Samathy Barratt <samathy.barratt@osirium.com>
2020-07-09 15:27:09 +0100

    Use pathlib magic to find requirements.in files if requirements.txt is passed in

--- pipautocompile.py
+++ pipautocompile.py
@@ -1,11 +1,16 @@
 #/usr/bin/env python
 import argparse
 import sys
+try:
+    import pathlib
+except ImportError:
+    import pathlib2 as pathlib
+
 from piptools.scripts import compile
 from piptools.exceptions import PipToolsError
 
 def compile_file(filename, pip_args):
-    compile.cli(["-r", filename, "-o", filename[0:-2]+"txt", "--verbose", "--generate-hashes"]+pip_args)
+    compile.cli(["-r", str(filename), "-o", str(filename)[0:-2]+"txt", "--verbose", "--generate-hashes"]+pip_args)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -14,35 +19,20 @@ def main():
 
     args = parser.parse_args(sys.argv)
 
-    files = args.files[1:]
+    files = sorted({pathlib.Path(p).with_suffix(".in") for p in args.files[1:]})
     pip_args = []
     if ( args.pip_compile_arg):
         pip_args = args.pip_compile_arg
 
     for f in files:
-        if f.endswith(".txt"):
-            # Attempt to find a matching in file if we changed a requirements.txt
-            requirementsin = f[0:-3]+"in"
-            if requirementsin not in files:
-                try:
-                    print("Compiling {}".format(requirementsin))
-                    compile_file(requirementsin, pip_args)
-                except SystemExit as e:
-                    if (e.code != 0):
-                        print("Attempted to compile maching `.in` to {}".format(f))
-                        print("Could not compile {}".format(requirementsin))
-                        print("Perhaps {} doesnt exist".format(requirementsin))
-                    continue
-        else:
-            print("non txt file")
-            try:
-                compile_file(f, pip_args)
-            except SystemExit as e:
-                if (e.code != 0):
-                    print(e)
-                    print("Could not compile {}".format(f))
-                    return False
-                continue
+        try:
+            compile_file(f, pip_args)
+        except SystemExit as e:
+            if (e.code != 0):
+                print(e)
+                print("Could not compile {}".format(f))
+                return False
+            continue
 
 if __name__ == "__main__":
     main()
